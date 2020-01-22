8fe526d71330a524bee8b24424d57d742f6845f4
AzatArtaAlmaty <54435719+AzatArtaAlmaty@users.noreply.github.com>
2020-01-22 14:16:39 +0600

    Update SavingDataFromDynamicTable.js
    добавление комментариев

--- scripts/SavingDataFromDynamicTable.js
+++ scripts/SavingDataFromDynamicTable.js
@@ -1,19 +1,19 @@
-jQuery(view.container).children("[innerId='deal']").click(() => {
+jQuery(view.container).children("[innerId='deal']").click(() => { //данные сохроняються по нажатию кнопки
     let data = [];
-    AS.FORMS.ApiUtils.simpleAsyncGet('rest/api/registry/create_doc', false, 'json', {
-        registryCode: 'purchase_requisition_shavketov_eldar1'
+    AS.FORMS.ApiUtils.simpleAsyncGet('rest/api/registry/create_doc', false, 'json', {//идет запрос на создание нового документа в реестре
+        registryCode: 'purchase_requisition_shavketov_eldar1'//код реестра
     }).then(response => response)
         .then(newDocument => {
-            AS.FORMS.ApiUtils.simpleAsyncGet('rest/api/asforms/data/get?dataUUID=' + newDocument.dataUUID).then(ASFdata => {
-                ASFdata[0].data.forEach(Aitem => {
-                    if (Aitem.id == "juridical")
+            AS.FORMS.ApiUtils.simpleAsyncGet('rest/api/asforms/data/get?dataUUID=' + newDocument.dataUUID).then(ASFdata => {// запрос на получение данных о документе
+                ASFdata[0].data.forEach(Aitem => {//итерация по компонентам формы
+                    if (Aitem.id == "juridical") // если код компонента juridical по изменяеться значение value на значение компонента
                         Aitem.value = model.playerModel.getModelWithId('juridical').getValue();
                     if (Aitem.id == "production") {
                         Aitem.data.forEach(item => {
                             if (item.type == "label")
                                 data.push(item)
                         })
-                        model.playerModel.getModelWithId('production').modelBlocks.forEach(a => {
+                        model.playerModel.getModelWithId('production').modelBlocks.forEach(a => {//итерация по компонентам дин таблицы
                             a.forEach(b => {
                                 switch (b.asfProperty.type) {
                                     case "custom":
@@ -33,10 +33,10 @@ jQuery(view.container).children("[innerId='deal']").click(() => {
                         })
                         Aitem.data = data;
                         console.log(data)
-                        AS.FORMS.ApiUtils.simpleAsyncPost('rest/api/asforms/data/save?uuid=' + newDocument.dataUUID, false, 'json', {
+                        AS.FORMS.ApiUtils.simpleAsyncPost('rest/api/asforms/data/save?uuid=' + newDocument.dataUUID, false, 'json', {//запрос на сохранение данных формы
                             data: '"data":' + JSON.stringify(ASFdata[0].data)
                         }).then(re => {
-                            AS.FORMS.ApiUtils.simpleAsyncGet('rest/api/registry/activate_doc', false, 'json', {
+                            AS.FORMS.ApiUtils.simpleAsyncGet('rest/api/registry/activate_doc', false, 'json', {// активация записи реестра по маршруту
                                         dataUUID: newDocument.dataUUID
                                     }).then(response => {
                                         $(".ui-widget-overlay.ui-front")[0].remove();
